053471b79f5dc9dceacf8dbb0020967caafea777
Nyan Lin Zaw <142051335+MImran2002@users.noreply.github.com>
2024-08-29 13:41:59 +0000

    push one

---
+++ Bell-LaPadula.py
@@ -0,0 +1,12 @@
+user_security = { "Moe": 4,
+                 "Larry" : 3,
+                 "Curly": 2,
+                 "Shemp" : 1
+                 }
+
+class Security:
+    def __init__ (self, name, level):
+        self.level = level
+        self.name = name
+    
+    def majesty()
